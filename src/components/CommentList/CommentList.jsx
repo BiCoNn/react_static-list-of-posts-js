683978f3a1c3d497936f3af830490d6e5087433e
Nazar <nazar.shevchuk.121@gmail.com>
2025-09-30 14:41:14 +0300

    sol1

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,1 +1,16 @@
-export const CommentList = () => <>Put the list here</>;
+import { CommentInfo } from '../CommentInfo';
+import './CommentList.scss';
+
+export const CommentList = ({ post }) => {
+  const hasComments = post.comments.length > 0;
+
+  return hasComments ? (
+    <div className="CommentList">
+      {post.comments.map(comment => (
+        <CommentInfo key={comment.id} post={{ comment }} />
+      ))}
+    </div>
+  ) : (
+    <b data-cy="NoCommentsMessage">No comments yet</b>
+  );
+};
--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,1 +1,12 @@
-export const UserInfo = () => <>Put the user here</>;
+// import { PostInfo } from '../PostInfo';
+import './UserInfo.scss';
+
+export const UserInfo = ({ post }) => {
+  const { user } = post;
+
+  return (
+    <a className="UserInfo" href={`mailto:${user.email}`}>
+      {user.name}
+    </a>
+  );
+};
